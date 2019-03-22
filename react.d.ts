071d206d087b73b0de2e725041e93935efdb407e
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-03-22 16:27:00 -0300

    Add comment over exporting types from Jest
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- react.d.ts
+++ react.d.ts
@@ -1,2 +1,5 @@
 export * from 'react-testing-library';
+// Exporting type definitions for Jest so that TypeScript Compiler can find it.
+// This is necessary for testing projects that use TypeScript.
+// Trying to find a better solution, check (https://github.com/vtex/test-tools/issues/7)
 export * from '@types/jest';
