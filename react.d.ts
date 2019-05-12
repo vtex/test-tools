1be73feda4950ee826799f184b7d5e713220d906
Breno Calazans <brenocalazans@gmail.com>
2019-05-12 20:34:33 -0300

    Add types for locale, messages and graphql options

--- react.d.ts
+++ react.d.ts
@@ -1,5 +1,33 @@
-export * from 'react-testing-library';
+import { RenderOptions, RenderResult } from "react-testing-library";
+import { MockedProviderProps } from "react-apollo/test-utils";
+
+export * from "react-testing-library";
+
+/**
+ *
+ */
+interface Messages {
+  [id: string]: string;
+}
+
+interface TestToolsRenderOptions extends RenderOptions {
+  /** Props to be passed to MockedProvider from react-apollo. */
+  graphql?: MockedProviderProps;
+  /** A locale string, eg: `pt-BR`, `es`. Default: `en`*/
+  locale?: string;
+  /** A JSON translation to be used. Default: `messages/en.json` or another locale if specified in the `locale` option. */
+  messages?: Messages;
+}
+
+/**
+ * Render into a container which is appended to document.body. It should be used with cleanup.
+ */
+export function render(
+  ui: React.ReactElement<any>,
+  options?: TestToolsRenderOptions
+): RenderResult;
+
 // Exporting type definitions for Jest so that TypeScript Compiler can find it.
 // This is necessary for testing projects that use TypeScript.
 // Trying to find a better solution, check (https://github.com/vtex/test-tools/issues/7)
-export * from '@types/jest';
+export * from "@types/jest";
