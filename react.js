c450c43d28cb2f691ab67540ca8e935d787a24e0
Felipe Sales <sales.felipeb@gmail.com>
2019-11-07 15:36:48 -0300

    Implement cache function similiar to the one that we use on render-runtime.

--- react.js
+++ react.js
@@ -3,18 +3,34 @@ const reactTestingLibrary = require('@testing-library/react')
 const { IntlProvider } = require('react-intl')
 const { MockedProvider } = require('react-apollo/test-utils')
 const { path, insertAll, reject, isNil, find } = require('ramda')
+const { InMemoryCache } = require('apollo-cache-inmemory')
+
 const paths = require('./modules/paths')
 
 const pkg = require(paths.resolveAppPath('package.json'))
 
-const getLocale = (optionsLocale) => {
+const getLocale = optionsLocale => {
   const defaultLocales = ['en', 'en-US']
   const pkgLocale = path(['vtexTestTools', 'defaultLocale'], pkg)
 
-  const locales = reject(isNil, insertAll(optionsLocale, pkgLocale, defaultLocales, []))
+  const locales = reject(
+    isNil,
+    insertAll(optionsLocale, pkgLocale, defaultLocales, [])
+  )
   const localeExists = locale => paths.pathExists(`../messages/${locale}.json`)
 
   return find(localeExists, locales)
+}
+
+// Creating apollo-client cache like render-runtime
+
+const generateCacheKey = value => {
+  const { cacheId, __typename } = value || {}
+  if (value && cacheId && __typename) {
+    return `${__typename}:${cacheId}`
+  }
+
+  return null
 }
 
 const customRender = (node, options = {}) => {
@@ -32,33 +48,36 @@ const customRender = (node, options = {}) => {
     ? Object.assign({}, { addTypename: false }, options.graphql)
     : { mocks: [], addTypename: false }
 
+  const cache = new InMemoryCache({
+    addTypename: apolloProps.addTypename,
+    dataIdFromObject: generateCacheKey,
+  })
+
+  apolloProps.cache = cache
+
   const rendered = reactTestingLibrary.render(
     React.createElement(
       IntlProvider,
       intlProps,
-      React.createElement(
-        MockedProvider,
-        apolloProps,
-        node
-      ),
+      React.createElement(MockedProvider, apolloProps, node)
     ),
     options
   )
 
- return {
-   ...rendered,
-   rerender: newUi =>
-     customRender(newUi, {
-       container: rendered.container,
-       baseElement: rendered.baseElement,
-     }),
- }
+  return {
+    ...rendered,
+    rerender: newUi =>
+      customRender(newUi, {
+        container: rendered.container,
+        baseElement: rendered.baseElement,
+      }),
+  }
 }
 
 const flushPromises = () => new Promise(resolve => setImmediate(resolve))
 
 // re-export everything
-module.exports = Object.assign({},
- reactTestingLibrary,
- { render: customRender, flushPromises }
-)
+module.exports = Object.assign({}, reactTestingLibrary, {
+  render: customRender,
+  flushPromises,
+})
