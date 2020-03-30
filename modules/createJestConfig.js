224ac9f4f0d77a799ce91c9309c4f68417909f8a
Maik <stiven3345@hotmail.com>
2020-03-30 09:22:26 -0500

    Get the coverageThreshold configuration from package.json file
    Read the pkg configuration and get coverageThreshold for pass to the jest configuration. This will be used to configure minimum threshold enforcement for coverage results

--- modules/createJestConfig.js
+++ modules/createJestConfig.js
@@ -11,21 +11,24 @@ module.exports = (resolve, resolveAppPath) => {
   if (pkg.jest && pkg.jest.setupFilesAfterEnv) {
     setupFilesAfterEnv = setupFilesAfterEnv.concat(pkg.jest.setupFilesAfterEnv)
   }
+  const coverageThreshold =
+    pkg.jest && pkg.jest.coverageThreshold ? pkg.jest.coverageThreshold : {}
 
   let config = {
     rootDir: resolveAppPath('.'),
     setupFilesAfterEnv: setupFilesAfterEnv,
+    coverageThreshold: coverageThreshold,
     moduleNameMapper: {
-      "^.+\\.css$": require.resolve('identity-obj-proxy'),
-      "^react$": require.resolve('react'),
+      '^.+\\.css$': require.resolve('identity-obj-proxy'),
+      '^react$': require.resolve('react'),
     },
     transform: {
-      "\\.(gql|graphql)$": require.resolve('jest-transform-graphql'),
-      "^.+\\.(js|jsx|mjs|ts|tsx)$": resolve('modules/jest/babelTransform.js'),
+      '\\.(gql|graphql)$': require.resolve('jest-transform-graphql'),
+      '^.+\\.(js|jsx|mjs|ts|tsx)$': resolve('modules/jest/babelTransform.js'),
       '^(?!.*\\.(js|jsx|mjs|css|ts|tsx|json|graphql|gql)$)': resolve(
         'modules/jest/fileTransform.js'
       ),
-    }
+    },
   }
 
   return config
