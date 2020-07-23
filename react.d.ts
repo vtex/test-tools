f4b06baed737fa8612644967b7b6792ae2078a7a
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-07-23 17:04:20 -0300

    Fix eslint issue

--- react.d.ts
+++ react.d.ts
@@ -1,37 +1,39 @@
-import { RenderOptions, RenderResult } from "@testing-library/react";
-import { MockedProviderProps } from "@apollo/react-testing";
+import { RenderOptions, RenderResult } from '@testing-library/react'
+import { MockedProviderProps } from '@apollo/react-testing'
 
-export * from "@testing-library/react";
-export { renderHook } from "@testing-library/react-hooks";
+export * from '@testing-library/react'
+export { renderHook } from '@testing-library/react-hooks'
 
 /**
  *
  */
 interface Messages {
-  [id: string]: string;
+  [id: string]: string
 }
 
-type TestToolsRenderOptions = BaseTestToolsRenderOptions | GraphQLTestToolsRenderOptions
+type TestToolsRenderOptions =
+  | BaseTestToolsRenderOptions
+  | GraphQLTestToolsRenderOptions
 
 interface BaseTestToolsRenderOptions extends RenderOptions {
-  /** A locale string, eg: `pt-BR`, `es`. Default: `en`*/
-  locale?: string;
+  /** A locale string, eg: `pt-BR`, `es`. Default: `en` */
+  locale?: string
   /** A JSON translation to be used. Default: `messages/en.json` or another locale if specified in the `locale` option. */
-  messages?: Messages;
+  messages?: Messages
 }
 
 interface GraphQLTestToolsRenderOptions extends BaseTestToolsRenderOptions {
   /** Props to be passed to MockedProvider */
-  graphql: MockedProviderProps;
+  graphql: MockedProviderProps
 }
 
 /**
  * Render into a container which is appended to document.body. It should be used with cleanup.
  */
 export function render(
-  ui: React.ReactElement<any>,
+  ui: React.ReactElement,
   options?: TestToolsRenderOptions
-): RenderResult;
+): RenderResult
 
 /**
  * Run a Promise that resolves in the next tick.
@@ -41,4 +43,4 @@ export function flushPromises(): Promise<void>
 // Exporting type definitions for Jest so that TypeScript Compiler can find it.
 // This is necessary for testing projects that use TypeScript.
 // Trying to find a better solution, check (https://github.com/vtex/test-tools/issues/7)
-export * from "@types/jest";
+export * from '@types/jest'
