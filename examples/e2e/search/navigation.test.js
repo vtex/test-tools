300807c355630085685e34dfa40b5e61ec3ba08d
Vitor Gomes <vitorlgomes@hotmail.com>
2021-03-02 15:13:25 -0300

    Change writing

--- examples/e2e/search/navigation.test.js
+++ examples/e2e/search/navigation.test.js
@@ -2,12 +2,12 @@ import { goToSearchPage } from '@vtex/test-tools/cypress'
 
 describe('Navigation between search pages', () => {
   before(() => {
-    goToSearchPage({ random: 1 })
+    goToSearchPage({ random: true })
   })
 
   it('navigates to another search page', () => {
     cy.getById('breadcrumb')
 
-    goToSearchPage({ random: 1 })
+    goToSearchPage({ random: true })
   })
 })
